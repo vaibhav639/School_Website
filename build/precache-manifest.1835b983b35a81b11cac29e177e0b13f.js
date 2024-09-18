75b02bcbc224a72ea3737dc02d11a47348afae5a
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-09-18 07:38:34 +0000

    adding readme changes

---
+++ build/precache-manifest.1835b983b35a81b11cac29e177e0b13f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bb77c9c57efaacd79fd",
+    "url": "/static/css/main.263b2b6a.chunk.css"
+  },
+  {
+    "revision": "2bb77c9c57efaacd79fd",
+    "url": "/static/js/main.2bb77c9c.chunk.js"
+  },
+  {
+    "revision": "ae4134845d08cf5df06c",
+    "url": "/static/js/1.ae413484.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3be9fca6c83d317697f3e44a27f250c6",
+    "url": "/static/media/logo2.3be9fca6.png"
+  },
+  {
+    "revision": "98d84046847599e5256d626868dfa414",
+    "url": "/static/media/slideImage.98d84046.png"
+  },
+  {
+    "revision": "ee63232fbac434a053dd97aa9275a026",
+    "url": "/static/media/ind1.ee63232f.jpeg"
+  },
+  {
+    "revision": "2526d232c2196bb14210d37c096148e1",
+    "url": "/static/media/ind2.2526d232.jpeg"
+  },
+  {
+    "revision": "5d0443d740a86e1b5b1c3c02e404a72b",
+    "url": "/static/media/ind3.5d0443d7.jpeg"
+  },
+  {
+    "revision": "7f7bf92d6e50fa39034086d7bd1833ec",
+    "url": "/static/media/pic-1.7f7bf92d.jpg"
+  },
+  {
+    "revision": "b428c59ff2c0e4978c603df1e83d1290",
+    "url": "/static/media/pic-2.b428c59f.jpg"
+  },
+  {
+    "revision": "f99cefbb5f77923501726f423549f607",
+    "url": "/static/media/pic-3.f99cefbb.jpg"
+  },
+  {
+    "revision": "97dad456fcf6b14738b3cb7667e4bda0",
+    "url": "/static/media/pic-4.97dad456.jpg"
+  },
+  {
+    "revision": "272d0dad7bca598c88ac9ebe329a25bf",
+    "url": "/static/media/pic-5.272d0dad.jpg"
+  },
+  {
+    "revision": "ba7debae3a3ae1358f6eaa861e0b7334",
+    "url": "/static/media/mobileApp.ba7debae.png"
+  },
+  {
+    "revision": "71a9ec66d16c569df4b0c8d7d168ec01",
+    "url": "/index.html"
+  }
+];
